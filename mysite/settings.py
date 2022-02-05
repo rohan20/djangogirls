d4615c03b43f2eb0b743bff26f04a64f2260e8e6
rohan20 <rhntnj@gmail.com>
2022-02-05 20:02:48 +0100

    change ALLOWED_HOSTS to allow pythonanywhere.com

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qvenbh%(z2*^m!x4q^&gtj&048&5s^p7lt@9h*ta!tr1xl*li2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
