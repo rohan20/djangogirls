e599a6e2590072a9de1ac5ea46bda8496cff324b
rohan20 <rhntnj@gmail.com>
2022-03-16 04:07:39 +0100

    redirect to post detail page after form submission succeeds

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -25,6 +25,8 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
+
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
 
